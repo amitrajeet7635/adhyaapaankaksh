eb39290194ce6448f06f96337675be35a75eb533
Amitrajeet <contact.forgeup@gmail.com>
2025-01-04 21:04:50 +0530

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   base: '/', // Add this to ensure correct base path handling for routing
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      input: '/src/main.jsx', 
-    },
-  },
   server: {
     host: true,
   },
