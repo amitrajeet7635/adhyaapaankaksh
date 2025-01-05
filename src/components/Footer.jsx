b023356703f04952eae314c5800a7d36eb3481f9
Amitrajeet <contact.forgeup@gmail.com>
2025-01-05 11:29:58 +0530

    footer background changed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ function Footer() {
         <>
 
 
-            <footer class="bg-white dark:bg-gray-900">
+            <footer class="bg-gray-900 dark:bg-gray-900">
                 <div class="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
                     <div class="md:flex md:justify-between">
                         <div class="mb-6 md:mb-0 flex flex-col gap-5">
